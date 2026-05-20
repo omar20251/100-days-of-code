3cb52e7b1b9974157216bf9de74338b8b38a7694
omar20251 <omarchaabane200@gmail.com>
2026-05-20 17:21:36 +0100

    initial commit - snake game - added the up,down,left and right functions

---
+++ snake_movement.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+tim = Turtle()
+tim.pensize(20)
+
+tim.forward(80)
+tim.bk(50)
+tim.left(90)
+tim.forward(80)
+tim.right(180)
+tim.forward(180)
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
