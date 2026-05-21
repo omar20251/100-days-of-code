479c31195294c8a8d2431d9eae418d16c5248215
omar20251 <omarchaabane200@gmail.com>
2026-05-21 19:33:01 +0100

    create a scoreboard and keep score

---
+++ scoreboard.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Arial",15, "normal")
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.goto(0, 275)
+        self.hideturtle()
+        self.color("white")
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"score = {self.score}", align = ALIGNMENT, font = FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
